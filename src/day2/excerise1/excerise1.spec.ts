2fef86c0f0b20999751b711e9884f670c8df3210
shruti_hegde <shrutihegde17aug@gmail.com>
2023-12-03 11:25:38 +0530

    Day 2 part 1 TDD setup

---
+++ src/day2/excerise1/excerise1.spec.ts
@@ -0,0 +1,37 @@
+import {describe, expect, test} from '@jest/globals';
+import { getSumOfPossibleGameIDs } from './excerise1';
+
+describe('Day 2 --> Excerise 1', () => {
+  test('should returns 0 when string is empty', () => {
+    expect(getSumOfPossibleGameIDs('', {
+      red: 12,
+      green: 13,
+      blue: 14
+    }))
+      .toBe(0);
+  });
+  test('should returns 0 when configuration is empty', () => {
+    expect(getSumOfPossibleGameIDs(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`, {}))
+      .toBe(0);
+  });
+
+  test('should returns sum ofPOSSIBLE GAME ID', () => {
+    expect(getSumOfPossibleGameIDs(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`, {
+      red: 12,
+      green: 13,
+      blue: 14
+    }))
+      .toBe(142);
+  });
+
+    
+
+});
---
+++ src/day2/excerise1/excerise1.ts
@@ -0,0 +1,9 @@
+import {data} from './input';
+export function getSumOfPossibleGameIDs (input:string, configuration: any): number {
+
+    return 0;
+}
+
+console.log('Day 2 excerise 1 o/p :', getSumOfPossibleGameIDs(data, {
+    red: 12, green: 13, blue: 14
+}));
