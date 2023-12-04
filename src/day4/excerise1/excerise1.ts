9ced275a5f8145b79354d88320ac51a27f9c9f15
shruti_hegde <shrutihegde17aug@gmail.com>
2023-12-04 12:46:40 +0530

    Day 4 part 1 test case

---
+++ src/day4/excerise1/excerise1.ts
@@ -0,0 +1,10 @@
+import {data} from './input';
+export function getPoints (input:string): number {
+    if (input) {
+
+    }
+    return 0;
+    
+}
+
+console.log('Day 4 excerise 1 o/p :', getPoints(data));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './day1/index';
 export * from './day2/index';
 export * from './day3/index';
+export * from './day4/index';
