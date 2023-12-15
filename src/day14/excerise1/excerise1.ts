624e99d15a8771bcdeffdd9c214ada0dac76f90c
shruti_hegde <shrutihegde17aug@gmail.com>
2023-12-15 11:52:04 +0530

    Pat 2

--- src/day14/excerise1/excerise1.ts
+++ src/day14/excerise1/excerise1.ts
@@ -6,32 +6,102 @@ export function getTotalLoad (input:string): number {
      const pattern = input.trim().split('\n').map((line:string) => {
         return line.trim().split('');
      });
-
-
+     let count  = 1;        
      const value = pattern.length;
-     let emptyIndex: number[] = [];
-     for (let col = 0;  col < pattern[0].length; col++) {
-        for (let row = 0;  row < value; row++) {
-            if (pattern[row][col] === '.') {
-                emptyIndex.push(row);
-            } else if (pattern[row][col] === 'O') {
-                let newrow;
-                if (emptyIndex.length > 0) {
-                    newrow = emptyIndex.shift() as number;
+     let prevPattern = '';
+     while (count <= 10000000 && prevPattern !== pattern.join('\n')) {
+        prevPattern = pattern.join('\n'); total = 0;
+        let emptyIndex: number[] = [];
+        for (let col = 0;  col < pattern[0].length; col++) {
+            for (let row = 0;  row < value; row++) {
+                if (pattern[row][col] === '.') {
                     emptyIndex.push(row);
-                    pattern[newrow][col] = 'O';
-                    pattern[row][col] = '.';
+                } else if (pattern[row][col] === 'O') {
+                    let newrow;
+                    if (emptyIndex.length > 0) {
+                        newrow = emptyIndex.shift() as number;
+                        emptyIndex.push(row);
+                        pattern[newrow][col] = 'O';
+                        pattern[row][col] = '.';
+                    }
+                } else  {
+                    emptyIndex = [];
                 }
-                total += (value - (newrow === undefined ? row : newrow));
-            } else  {
-                emptyIndex = [];
+            }
+            emptyIndex = [];
+        }
+
+        emptyIndex = [];
+        for (let row = 0;  row < value; row++) {
+            for (let col = 0;  col < pattern[0].length; col++) {
+                if (pattern[row][col] === '.') {
+                    emptyIndex.push(col);
+                } else if (pattern[row][col] === 'O') {
+                    let newCol;
+                    if (emptyIndex.length > 0) {
+                        newCol = emptyIndex.shift() as number;
+                        emptyIndex.push(col);
+                        pattern[row][newCol] = 'O';
+                        pattern[row][col] = '.';
+                    }
+                } else  {
+                    emptyIndex = [];
+                }
+            }
+            emptyIndex = [];
+        }
+
+        emptyIndex = [];
+        for (let col = 0;  col < pattern[0].length; col++) {
+            for (let row = value - 1;  row >= 0; row--) {
+                if (pattern[row][col] === '.') {
+                    emptyIndex.push(row);
+                } else if (pattern[row][col] === 'O') {
+                    let newrow;
+                    if (emptyIndex.length > 0) {
+                        newrow = emptyIndex.shift() as number;
+                        emptyIndex.push(row);
+                        pattern[newrow][col] = 'O';
+                        pattern[row][col] = '.';
+                    }
+                } else  {
+                    emptyIndex = [];
+                }
+            }
+            emptyIndex = [];
+        }
+            
+        emptyIndex = [];
+        for (let row = 0;  row < value; row++) {
+            for (let col = pattern[0].length - 1;  col >= 0; col--) {
+                if (pattern[row][col] === '.') {
+                    emptyIndex.push(col);
+                } else if (pattern[row][col] === 'O') {
+                    let newCol;
+                    if (emptyIndex.length > 0) {
+                        newCol = emptyIndex.shift() as number;
+                        emptyIndex.push(col);
+                        pattern[row][newCol] = 'O';
+                        pattern[row][col] = '.';
+                    }
+                } else  {
+                    emptyIndex = [];
+                }
+            }
+            emptyIndex = [];
+        }
+        count++;
+          
+    for (let col = 0;  col < pattern[0].length; col++) {
+        for (let row = 0;  row < value; row++) { 
+            if (pattern[row][col] === 'O') {
+                total += (value - row);
             }
         }
-        emptyIndex = [];
-     }
-         
-     console.log(pattern.join('\n'));       
-        
+    }
+    }
+  
+
         return total;
     }
     return 0;
@@ -39,5 +109,5 @@ export function getTotalLoad (input:string): number {
 
 
 
-console.log('Day 14 excerise 1 o/p :', getTotalLoad(data));
+//console.log('Day 14 excerise 1 o/p :', getTotalLoad(data));
 
